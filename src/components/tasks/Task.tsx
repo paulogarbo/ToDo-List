2c6b7f11fe1c86ee7702ed232b02f5602f32db90
Paulo <paulexgarbo@gmail.com>
2024-04-25 22:19:31 -0300

    finish css

--- src/components/tasks/Task.tsx
+++ src/components/tasks/Task.tsx
@@ -1,17 +1,34 @@
+import { useState } from 'react'
 import styles from './Task.module.css'
-import { Trash2 } from 'lucide-react'
+import { Trash2, Check, Bold } from 'lucide-react'
+import * as Checkbox from '@radix-ui/react-checkbox'
 
 export function Task() {
+  const [ checked, setChecked ] = useState(false)
+
+  function handleChangeChecked() {
+    if (checked) {
+      setChecked(false)
+      return
+    }
+    setChecked(true)
+  }
+
   return (
-    <div className={styles.tasksWrapper}>
-      <button className={styles.checkedTask}></button>
-      <p>
-        Integer urna interdum massa libero auctor neque turpis turpis semper. 
-        Duis vel sed fames integer.
-      </p>
-      <button className={styles.removeTask}>
-        <Trash2 size={16}/>
-      </button>
-    </div>
+    <section className={styles.container}>
+      <div>
+        <Checkbox.Root className={checked ? styles.checkedBox : styles.checkBox} onClick={handleChangeChecked}>
+          <span>
+            <Check className={styles.vetor}/>
+          </span>
+        </Checkbox.Root>
+        <p>
+          Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+        </p>
+        <button className={styles.removeTask}>
+          <Trash2 size={18}/>
+        </button>
+      </div>
+    </section>
   )
 }
